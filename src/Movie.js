4cc89b351e4a72377e2c1d08b5ee97e26d90616d
woori3104 <sizuku11261@gmail.com>
2020-05-23 19:17:46 +0900

    버그수정

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,7 @@ import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div class='movie'>
+    <div className='movie'>
       <img src={poster} alt={title} title={title} />
       <div className='movie__data'>
         <h3 className='movie__title'>{title}</h3>
